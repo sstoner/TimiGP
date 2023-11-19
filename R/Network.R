0a61ff098b9cb22b2ab4f4fc97e4a27f0c72ac6e
Vancl <liyannois@gmail.com>
2023-11-20 00:56:27 +0800

    fixup
    Signed-off-by: Vancl <liyannois@gmail.com>

--- R/Network.R
+++ R/Network.R
@@ -521,7 +521,7 @@ TimiGeneNetwork<-  function(resdata = NULL,
     if(sum(unique(geneset$Gene) %in% selected) == 0) {
       stop('No Gene were found in given geneset')
     } else{
-      se <- which(geneset[,2] %in% selected)
+      se <- which(geneset$Gene %in% selected)
       node <- geneset[se,]
     }
     
